6c68fb24230e7e3844b02007a5cb1783855468da
anhhung007v3 <thucinsulation.nl@gmail.com>
2026-07-18 08:29:41 +0700

    add SHD main runtime entry V2

--- main_runtime.py
+++ main_runtime.py
@@ -1,36 +1,20 @@
 """
 Trading Brain SHD
-Runtime Entry Point V1
+Main Runtime V2
+
+Real runtime entry point.
 """
 
 
+from brain.runtime.runtime_controller import RuntimeController
 from brain.runtime.runtime_service import RuntimeService
 from brain.scheduler.runtime_scheduler import RuntimeScheduler
-
-
-class DummyController:
-    """
-    Temporary controller adapter.
-
-    RuntimeController sẽ được inject thật sau.
-    """
-
-    def startup(self):
-        print("SHD STARTUP")
-
-
-    def run_cycle(self):
-        print("SHD CYCLE")
-
-
-    def shutdown(self):
-        print("SHD SHUTDOWN")
 
 
 
 def main():
 
-    controller = DummyController()
+    controller = RuntimeController()
 
 
     scheduler = RuntimeScheduler(
@@ -40,7 +24,8 @@ def main():
 
 
     service = RuntimeService(
-        scheduler
+        scheduler,
+        controller
     )
 
 
